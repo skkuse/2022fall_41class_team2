2b94592348f53c8b9f182401daac8abaed31483e
joonparkhere <tmdgh0221@gmail.com>
2022-10-22 18:40:13 +0900

    feat: Add test environment

--- backend/backend/settings/__init__.py
+++ backend/backend/settings/__init__.py
@@ -5,5 +5,7 @@ SETTINGS_MODULE = os.environ.get('DJANGO_SETTINGS_MODULE')
 
 if not SETTINGS_MODULE or SETTINGS_MODULE == 'backend.settings.dev' or SETTINGS_MODULE == 'backend.settings':
     from .dev import *
+elif SETTINGS_MODULE == 'backend.settings.test':
+    from .test import *
 elif SETTINGS_MODULE == 'backend.settings.prod':
     from .prod import *
---
+++ backend/backend/settings/test.py
@@ -0,0 +1,19 @@
+from .base import *
+
+import os
+import environ
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Environment variables definition
+env = environ.Env(
+    DEBUG=(bool, True)
+)
+environ.Env.read_env(os.path.join(BASE_DIR, '.test-env'))
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+WSGI_APPLICATION = 'backend.wsgi.dev.application'
