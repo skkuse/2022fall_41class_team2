88e17f491d63d76a73b2435cd60f640d84c4d45c
joonparkhere <tmdgh0221@gmail.com>
2022-10-22 22:39:34 +0900

    feat: Add a test case about delete an object

--- backend/lecture/test_models.py
+++ backend/lecture/test_models.py
@@ -7,29 +7,41 @@ from lecture.models import Lecture
 
 
 class TestLecture(TestCase):
-    mock_date = datetime(2022, 1, 1, 1, 1, 1, tzinfo=timezone.utc)
-    mock_name = 'mock-name'
-    mock_email = 'mock-email@email.com'
-    mock_nickname = 'mock-nickname'
-    mock_profile_image_url = 'https://mock-profile-image-url.com'
-    mock_github_api_url = 'https://mock-github-api-url.com'
+    mock_instructor_email = 'mock-instructor@email.com'
 
-    @patch('django.utils.timezone.now', return_value=mock_date)
+    @patch('django.utils.timezone.now', return_value=datetime(2022, 1, 1, 1, 1, 1, tzinfo=timezone.utc))
     def setUp(self, mock_now) -> None:
         User.objects.create(
-            name=self.mock_name,
-            email=self.mock_email,
-            nickname=self.mock_nickname,
-            profile_image_url=self.mock_profile_image_url,
-            github_api_url=self.mock_github_api_url,
+            name='mock-instructor-name',
+            email=self.mock_instructor_email,
+            nickname='mock-instructor-nickname',
+            profile_image_url='https://mock-instructor-profile-image-url.com',
+            github_api_url='https://mock-instructor-github-api-url.com',
             last_login=timezone.now(),
         )
 
-    def test_save_lecture(self):
-        name = "dummy_name"
-        instructor = User.objects.get(email=self.mock_email)
-        lecture = Lecture.objects.create(name=name, instructor=instructor)
+    def test_lecture_save(self):
+        lecture_name = "dummy-lecture-name"
+        instructor = User.objects.get(email=self.mock_instructor_email)
+
+        lecture = Lecture.objects.create(
+            name=lecture_name,
+            instructor=instructor
+        )
 
         self.assertIsNotNone(lecture.id)
-        self.assertEqual(lecture.name, name)
+        self.assertEqual(lecture.name, lecture_name)
         self.assertEqual(lecture.instructor, instructor)
+
+    def test_lecture_remove(self):
+        instructor = User.objects.get(email=self.mock_instructor_email)
+        lecture = Lecture.objects.create(
+            name='dummy-lecture-name',
+            instructor=instructor,
+        )
+
+        result = lecture.delete()
+        user = User.objects.get(email=self.mock_instructor_email)
+
+        self.assertEqual(result[1].get('id'), lecture.id)
+        self.assertIsNotNone(user)
