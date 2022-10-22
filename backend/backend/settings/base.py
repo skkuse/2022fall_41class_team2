33ee86b1b8bb04ab62ccebcb114f0883b0252f96
joonparkhere <tmdgh0221@gmail.com>
2022-10-22 17:23:36 +0900

    feat: Modify default permission class and Apply pagination class

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = 'authentication.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_RENDERER_CLASSES': [
         'backend.renderer.CustomRenderer',
@@ -53,7 +55,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'EXCEPTION_HANDLER': 'backend.handler.custom_exception_handler',
 }
