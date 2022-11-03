138aa3f09954e37c8835d5cc268bdfc7a78882b1
joonparkhere <tmdgh0221@gmail.com>
2022-11-03 18:37:16 +0900

    feat: Add programming languages execution part

--- backend/output/utils.py
+++ backend/output/utils.py
@@ -1,122 +1,182 @@
 import docker
 import os
-import tempfile
+import logging
 
 from uuid import uuid4
+from rest_framework import status
+from rest_framework.exceptions import APIException
 from docker.errors import DockerException, ContainerError
 
 from backend.settings.base import BASE_DIR
 
 SERVER_CODE_DIR = str(BASE_DIR) + os.environ['SERVER_CODE_DIR']
 CONTAINER_CODE_DIR = os.environ['CONTAINER_CODE_DIR']
+
+EXECUTED_FUNCTION_NAME = 'solution'
+
+SUPPORT_LANGUAGE = ['python', 'javascript', 'c', 'cpp']
+
 PYTHON_IMAGE = os.environ['PYTHON_IMAGE']
 NODE_IMAGE = os.environ['NODE_IMAGE']
+GCC_IMAGE = os.environ['GCC_IMAGE']
 
-SUPPORT_LANGUAGE = ['python', 'javascript']
-
-
-def execute_code(language: str, code: str, *args):
-    language = language.lower()
-
-    if language not in SUPPORT_LANGUAGE:
-        raise Exception
-
-    if language == 'python':
-        return execute_python(code, *args)
-    if language == 'javascript':
-        return execute_javascript(code, *args)
+logger = logging.getLogger(__name__)
 
 
-def execute_python(code: str, *args):
-    # TODO modify to use temporary file
+def generate_files(contents: [str], prefixes=None):
     try:
-        executable_code = setup_python_code(code, *args)
+        filenames = []
+        for idx, content in enumerate(contents):
+            if prefixes is None or prefixes[idx] is None:
+                filename = str(uuid4())
+            else:
+                filename = ''.join([str(uuid4()), prefixes[idx]])
 
-        tempfile.NamedTemporaryFile()
-        with tempfile.TemporaryFile() as file:
-            file.write(executable_code)
+            with open(SERVER_CODE_DIR + filename, 'w') as file:
+                file.write(content)
 
-        filename = str(uuid4())
-        with open(SERVER_CODE_DIR + filename, 'w') as file:
-            file.write(executable_code)
+            filenames.append(filename)
+        return filenames
+    except Exception as e:
+        logger.exception(e)
+        raise APIException(detail='Generating files are failed.', code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        client = docker.from_env()
-        volumes = {
-            SERVER_CODE_DIR: {'bind': CONTAINER_CODE_DIR, 'mode': 'ro'},
-        }
 
-        response = client.containers.run(
-            image=PYTHON_IMAGE,
-            command=f'python {filename}',
-            volumes=volumes,
-            working_dir=CONTAINER_CODE_DIR,
-            remove=True,
-        )
-        return {
-            'exit_status': 0,
-            'out': response.decode('utf-8'),
-        }
+def delete_files(filenames: [str]):
+    for filename in filenames:
+        full_filename = SERVER_CODE_DIR + filename
+        if os.path.isfile(full_filename):
+            os.remove(full_filename)
+
+
+def setup_python_code(raw_code: str):
+    return '\n\n\n'.join([raw_code, f'{EXECUTED_FUNCTION_NAME}()'])
+
+
+def setup_javascript_code(raw_code: str):
+    return '\n\n'.join([raw_code, f'{EXECUTED_FUNCTION_NAME}()'])
+
+
+def setup_c_code(raw_code: str):
+    return '\n\n'.join([raw_code, f'int main() {{ {EXECUTED_FUNCTION_NAME}(); return 0; }}'])
+
+
+def setup_cpp_code(raw_code: str):
+    return '\n\n'.join([raw_code, f'int main() {{ {EXECUTED_FUNCTION_NAME}(); return 0; }}'])
+
+
+def setup_input(raw_input: str):
+    return raw_input.strip()
+
+
+def run(language: str, raw_code: str, raw_input: str):
+    language = language.lower()
+    if language not in SUPPORT_LANGUAGE:
+        raise APIException(detail=f'{language} does not supported.', code=status.HTTP_400_BAD_REQUEST)
+
+    code_filename = None
+    input_filename = None
+
+    try:
+        if language == 'python':
+            [code_filename, input_filename] = generate_files([
+                setup_python_code(raw_code),
+                setup_input(raw_input),
+            ])
+            return execute_python(
+                code_filename=code_filename,
+                input_filename=input_filename,
+            )
+        elif language == 'javascript':
+            [code_filename, input_filename] = generate_files([
+                setup_javascript_code(raw_code),
+                setup_input(raw_input),
+            ])
+            return execute_javascript(
+                code_filename=code_filename,
+                input_filename=input_filename,
+            )
+        elif language == 'c':
+            [code_filename, input_filename] = generate_files([
+                setup_c_code(raw_code),
+                setup_input(raw_input),
+            ], prefixes=['.c', ''])
+            return execute_c(
+                code_filename=code_filename,
+                input_filename=input_filename,
+            )
+        elif language == 'cpp':
+            [code_filename, input_filename] = generate_files([
+                setup_cpp_code(raw_code),
+                setup_input(raw_input),
+            ], prefixes=['.cpp', ''])
+            return execute_cpp(
+                code_filename=code_filename,
+                input_filename=input_filename,
+            )
     except ContainerError as e:
         byte = e.stderr
         if not isinstance(byte, bytes):
             byte = '\n'.encode()
-        outs = byte.decode('utf-8').split('\n', 1)
+        output = byte.decode('utf-8')
 
-        tmp = outs[0].split(' ')[-1]
-        line_num = int(tmp) if tmp.isnumeric() else None
-        err_msg = outs[1]
+        output = output.replace(code_filename, 'code')
+        output = output.replace(input_filename, 'input')
 
         return {
             'exit_status': e.exit_status,
-            'out': {
-                'line_number': line_num,
-                'error_message': err_msg,
-            },
+            'output': output,
         }
     except DockerException as e:
-        raise e
+        logger.exception(e)
+        raise APIException(detail='Docker engine exception is occur.', code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    finally:
+        delete_files(filenames=[code_filename, input_filename])
 
 
-def execute_javascript(code: str, *args):
-    # TODO modify to use temporary file
-    try:
-        filename = 'script.js'
-        with open(SERVER_CODE_DIR + filename, 'w') as file:
-            file.write(code)
-
-        client = docker.from_env()
-        volumes = {
-            SERVER_CODE_DIR: {'bind': CONTAINER_CODE_DIR, 'mode': 'ro'},
-        }
-
-        response = client.containers.run(
-            NODE_IMAGE,
-            command='node ' + filename,
-            volumes=volumes,
-            working_dir=CONTAINER_CODE_DIR,
-            remove=True,
-        )
-        return response.decode('utf-8')
-    except DockerException as e:
-        print(e)
-        raise e
+def execute_python(code_filename: str, input_filename: str):
+    return execute_container(
+        image=PYTHON_IMAGE,
+        command=f'sh -c "python {code_filename} < {input_filename}"'
+    )
 
 
-def setup_python_code(code: str, *args):
-    params = make_params(*args)
-    return '\n\n\n'.join([code, f'print(solution({params}))'])
+def execute_javascript(code_filename: str, input_filename: str):
+    return execute_container(
+        image=NODE_IMAGE,
+        command=f'sh -c "node {code_filename} < {input_filename}"',
+    )
 
 
-def make_params(*args):
-    params = str()
-    for raw in args:
-        if isinstance(raw, (int, float, complex)):
-            params += f'{raw},'
-        elif isinstance(raw, str):
-            params += f'\'{raw}\','
-        elif isinstance(raw, list):
-            params += f'[{make_params(raw)}],'
-        else:
-            raise Exception
-    return params[:len(params) - 1]
+def execute_c(code_filename: str, input_filename: str):
+    return execute_container(
+        image=GCC_IMAGE,
+        command=f'sh -c "gcc -o main {code_filename} && ./main < {input_filename} && rm ./main"',
+    )
 
+
+def execute_cpp(code_filename: str, input_filename: str):
+    return execute_container(
+        image=GCC_IMAGE,
+        command=f'sh -c "g++ -o main {code_filename} && ./main < {input_filename} && rm ./main"',
+    )
+
+
+def execute_container(image: str, command: str):
+    client = docker.from_env()
+    volumes = {
+        SERVER_CODE_DIR: {'bind': CONTAINER_CODE_DIR, 'mode': 'rw'},
+    }
+    response = client.containers.run(
+        image=image,
+        command=command,
+        volumes=volumes,
+        working_dir=CONTAINER_CODE_DIR,
+        remove=True,
+        detach=False,
+    )
+
+    return {
+        'exit_status': 0,
+        'output': response.decode('utf-8'),
+    }
