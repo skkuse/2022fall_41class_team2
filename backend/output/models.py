ab4642ae0a199f9cb1f295f8384f11eb65869404
joonparkhere <tmdgh0221@gmail.com>
2022-11-12 12:55:37 +0900

    feat: Add output base model

--- backend/output/models.py
+++ backend/output/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Result(models.Model):
+    repo = models.ForeignKey('repo.Repo', on_delete=models.PROTECT, related_name='results')
+    references = models.JSONField(default=[])
+    # TODO @동우
+    code_description = models.TextField(default="")
+
+
+# TODO @승호
+class FunctionalityResult(models.Model):
+    result = models.ForeignKey('output.Result', on_delete=models.PROTECT, related_name='functionality_results')
+
+
+# TODO @동우
+class ReadabilityResult(models.Model):
+    result = models.ForeignKey('output.Result', on_delete=models.PROTECT, related_name='readability_results')
+
+
+# TODO @영준
+class EfficiencyResult(models.Model):
+    result = models.ForeignKey('output.Result', on_delete=models.PROTECT, related_name='efficiency_results')
+
+
+# TODO @영준
+class PlagiarismResult(models.Model):
+    result = models.ForeignKey('output.Result', on_delete=models.PROTECT, related_name='plagiarism_results')
