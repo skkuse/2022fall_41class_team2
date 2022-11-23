067a2c0931c94e414b2974dbddd4cca0dc7adc85
ByungWoo <63662808+bw-99@users.noreply.github.com>
2022-11-23 17:00:59 +0900

    Add: merge feature-api

--- frontend/src/sagas/LectureSaga.js
+++ frontend/src/sagas/LectureSaga.js
@@ -8,8 +8,41 @@ import { GET_LECTURES, GET_LECTURES_FAIL, GET_LECTURES_SUCCESS, LECTURE_ACTION_T
 
 
 async function getLectureAPI(payload) {
-    const result = await apiClient.get("/api/lectures/");
-    console.log(result);
+    let result = await apiClient.get("/api/lectures/");
+    if(result.status == 200 && result.data.data.results.length == 0) {
+        const postResult = await apiClient.post("/api/lectures/", {
+            name: "예시) 소프트웨어공학개론4"
+        });
+        result.data.data.results.push(postResult.data.id);
+    }
+
+    // apiClient.delete("/api/lectures/1/");
+    // apiClient.delete("/api/lectures/2/");
+    // alert(JSON.stringify(result.data.data));
+
+    for (const index in result.data.data.results) {
+        let lecture = result.data.data.results[index];
+        let assResult = await apiClient.get("/api/assignments/?lecture_id="+ lecture.id);
+        // alert(JSON.stringify(assResult));
+        console.log(assResult);
+        if(!assResult.data.data.results.length){
+            let postAssResult = await apiClient.post("/api/assignments/", {
+                "name": "Assignment1",
+                "deadline": "2022-11-18T12:45:25.465Z",
+                "question": "피보나치 수열을 만드시오.",
+                "constraints": "",
+                "contents" : [{
+                    "skeleton_code": "#include <stdio.h>",
+                    "answer_code": "#include <stdio.h>",
+                    "language": "python"
+                }],
+                "lecture_id": lecture.id,
+            });
+            assResult = await apiClient.get("/api/assignments/?lecture_id="+ lecture.id);
+        }
+        lecture.assignments = assResult.data.data;
+    }
+    console.log(result.data.data);
     return result.data.data;
 }
 
