44b0fcbdef3a093f926c47f6fb378120ef1f91d1
joonparkhere <tmdgh0221@gmail.com>
2022-11-25 21:51:49 +0900

    refactor: Clear un-necessary comments and import statements

--- backend/output/utils_efficiency.py
+++ backend/output/utils_efficiency.py
@@ -1,8 +1,8 @@
 import json
 import subprocess
+
 from output.serializers import EfficiencyResultSerializer
 from memory_profiler import memory_usage
-from statistics import mean
 
 LOC_FIELD = 'loc_score'
 CONTROL_FLOW_COMPLEXITY_FIELD = 'control_flow_complexity_score'
@@ -28,7 +28,6 @@ def run(result_id: int, full_filename: str):
 
 
 def execute_multimetric(full_filename: str):
-    # Run multimetric on a given file path
     process = subprocess.run(
         ['multimetric', f'{full_filename}'],
         stdout=subprocess.PIPE,
@@ -48,14 +47,11 @@ def execute_multimetric(full_filename: str):
         RESERVATION_WORDS_FIELD: r_words_score,
     }
 
-# todo: discuss whether we should use the avg, min, or max OR all three
-#       -> current implementation is with avg mem usage 
-#       -> "timeout" defines how long a program should be run before it is closed (set in case of non-terminating programs)
-#       -> "MB" should be appended to the df_complexity score -- dunno if this should be done in frontend or backend
+
 def execute_memory_profiler(full_filename: str):
     program = subprocess.Popen(["python", full_filename])
     mem_usage = memory_usage(proc=program, timeout=1)
-    df_complexity_score = round(max(mem_usage),2)
+    df_complexity_score = round(max(mem_usage), 2)
 
     return {
         DATA_FLOW_COMPLEXITY_FIELD: df_complexity_score,
